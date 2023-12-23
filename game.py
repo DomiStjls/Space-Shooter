a65fc41621e293479801ba00a141309f7071118a
MaksimKharlamov <101044403+MaksimKharlamov@users.noreply.github.com>
2023-12-23 19:26:42 +0300

    немного доработанный файл
    добавлены враги и чтение файлов уровня

--- game.py
+++ game.py
@@ -1,31 +1,32 @@
 import os
-
 import pygame
-
-# константы
-WIDTH = 600
-HEIGHT = 400
-FPS = 60
-SPEED = 10  # px/frame speed of player
 
 # глобальные переменные
 pygame.init()
+WIDTH, HEIGHT = 600, 400
+# WIDTH, HEIGHT = pygame.display.get_desktop_sizes()[0]
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
+
+# константы
+FPS = 60
+SPEED = 10  # px/frame speed of player
 
 player_group = pygame.sprite.Group()
 enemy_group = pygame.sprite.Group()
 bullet_group = pygame.sprite.Group()
+player_bullets = pygame.sprite.Group()
+enemy_bullets = pygame.sprite.Group()
 
 BULLETEVENT = pygame.USEREVENT + 1
 
 
 def load_image(name, color_key=-1):
-    fullname = os.path.join('data', name)
+    fullname = os.path.join("data", name)
     try:
         image = pygame.image.load(fullname)
     except pygame.error as message:
-        print('Cannot load image:', name)
+        print("Cannot load image:", name)
         raise SystemExit(message)
 
     if color_key is not None:
@@ -38,7 +39,24 @@ def load_image(name, color_key=-1):
 
 
 class Enemy(pygame.sprite.Sprite):
-    pass
+    types = [
+        (load_image("enemyr.png"), 1),
+        (load_image("enemyb.png"), 2),
+        (load_image("enemyg.png"), 3),
+    ]
+
+    def __init__(self, type, pos_x, pos_y):
+        super().__init__(enemy_group)
+        self.image, self.bul_speed = Enemy.types[type]
+        self.rect = self.image.get_rect().move(pos_x, pos_y)
+        self.rect.x -= self.rect.w // 2
+        self.rect.y -= self.rect.h // 2
+
+    def update(self):
+        global running
+        if pygame.sprite.spritecollideany(self, player_bullets):
+            enemy_group.remove(self)
+        Bullet("bullet.png", self.rect.x + self.rect.w // 2, self.rect.y + self.rect.h, 0, self.bul_speed)
 
 
 class Player(pygame.sprite.Sprite):
@@ -46,20 +64,39 @@ class Player(pygame.sprite.Sprite):
         super().__init__(player_group)
         self.image = load_image(image)
         self.rect = self.image.get_rect().move(pos_x, pos_y)
+        self.rect.x -= self.rect.w // 2
+        self.rect.y -= self.rect.h // 2
+
+    def update(self):
+        global running
+        if pygame.sprite.spritecollideany(self, enemy_bullets):
+            running = False
 
 
 class Bullet(pygame.sprite.Sprite):
-    def __init__(self, image, pos_x, pos_y, vx, vy):
+    def __init__(self, image, pos_x, pos_y, vx, vy, whose=0):  # whose = 0 - вражеская 1 - своя
         super().__init__(bullet_group)
+        if whose == 0:
+            enemy_bullets.add(self)
+        else:
+            player_bullets.add(self)
         self.image = load_image(image)
+        self.mask = pygame.mask.from_surface(self.image)
         self.rect = self.image.get_rect().move(pos_x, pos_y)
         self.vx = vx
         self.vy = vy
+        self.whose = whose
 
     def update(self):
+        global running
         self.rect.x += self.vx
         self.rect.y += self.vy
-        if self.rect.x + self.rect.w < 0 or self.rect.y + self.rect.h < 0 or self.rect.x > WIDTH or self.rect.y > HEIGHT:
+        if (
+                self.rect.x + self.rect.w < 0
+                or self.rect.y + self.rect.h < 0
+                or self.rect.x > WIDTH
+                or self.rect.y > HEIGHT
+        ):
             bullet_group.remove(self)
 
 
@@ -67,7 +104,18 @@ class Present(pygame.sprite.Sprite):
     pass
 
 
-player = Player("player.png", 200, 200)
+level = "q.txt"
+with open("data/" + level) as f:
+    q = f.readlines()
+    x = WIDTH / (len(q[0]) - 1)
+    y = HEIGHT / len(q)
+    for i, line in enumerate(q):
+        for j, char in enumerate(line[:-1]):
+            if char == "@":
+                player = Player("player.png", x * j + x / 2, y * i + y / 2)
+            elif char != ".":
+                Enemy(int(char), x * j + x / 2, y * i + y / 2)
+
 pygame.time.set_timer(BULLETEVENT, 500)
 vx = vy = 0
 running = True
@@ -94,7 +142,10 @@ while running:
             if event.key == pygame.K_DOWN:
                 vy -= SPEED
         if event.type == BULLETEVENT:
-            bullet = Bullet("bullet.png", player.rect.x, player.rect.y, 0, -10)
+            bullet = Bullet(
+                "bullet.png", player.rect.x + player.rect.w // 2, player.rect.y, 0, -20, 1
+            )
+            enemy_group.update()
 
     # движение игрока
     player.rect.x += vx
@@ -103,10 +154,13 @@ while running:
     player.rect.y = max(0, min(HEIGHT - player.rect.h, player.rect.y))
 
     screen.fill(255)
+    player_group.update()
     player_group.draw(screen)
 
     bullet_group.update()
     bullet_group.draw(screen)
+
+    enemy_group.draw(screen)
 
     pygame.display.flip()
     clock.tick(FPS)
